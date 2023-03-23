0a59378d9d2d3f87710240f92714036937ee2fdd
samir <nexxion.samir@gmail.com>
2023-03-23 13:12:56 +0100

    update all tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul(int a, int b) - return multiple of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0 or 1
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
