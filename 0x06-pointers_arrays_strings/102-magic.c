79e7c39386cdce88414afe03443fb365a7bca204
nexxion.samir@gmail.com <nexxion.samir@gmail.com>
2023-03-29 20:10:19 +0000

    upload advanced

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember: function (n get 2 bit and a 5 bites so: | n | n | a | a | a | a | a |, p get the adresse of n, so p[5] it will edit a[2])
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
