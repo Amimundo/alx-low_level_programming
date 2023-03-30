278ed359bd438a2324db26c0a771ba727953f96c
samir <nexxion.samir@gmail.com>
2023-03-30 16:06:08 +0200

    update task magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,13 +10,13 @@ int main(void)
   p = &n;
   /*
    * write your line of code here...
-   * Remember: function (n get 2 bit and a 5 bites so: | n | n | a | a | a | a | a |, p get the adresse of n, so p[5] it will edit a[2])
+   * Remember: function (n get 2 bit and a 5 bites so: | n | n | a0 | a0 | a1 | a1 | a2 | a2 |..., p get the adresse of n, so p[5] it will edit a[2])
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+  *(p + 7) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
